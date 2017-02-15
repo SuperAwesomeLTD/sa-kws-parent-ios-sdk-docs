ea0e1017b48a9bc3ce4e4791e37ca0f6d723c5e7
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-02-15 18:47:36 +0000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 BRANCH="$1"
 
 if [ $1 = "develop" ]; then
-    aws --region=us-west-1 s3 sync "build/html/" s3://doc.staging.superawesome.tv/sa-kws-android-sdk/
+    aws --region=us-west-1 s3 sync "build/html/" s3://doc.staging.superawesome.tv/sa-kws-parent-ios-sdk/
 else
-    aws --region=us-west-1 s3 sync "build/html/" s3://doc.superawesome.tv/sa-kws-android-sdk/
+    aws --region=us-west-1 s3 sync "build/html/" s3://doc.superawesome.tv/sa-kws-parent-ios-sdk/
 fi
